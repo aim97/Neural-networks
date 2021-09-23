bbe2c64de71ee5e666e01adac19ff10458019690
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-09-23 13:21:23 +0200

    improve results plotting

--- Tensorflow/callbacks.py
+++ Tensorflow/callbacks.py
@@ -1,6 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from math import floor
+
+
+class PlottingCallback(tf.keras.callbacks.Callback):
+  def __init__(self, epoch_step, steps):
+    super(PlottingCallback, self).__init__()
+    self.epoch_step = epoch_step
+    self.steps = steps
+
+    rows = (steps + 4) // 5
+    cols = 5
+
+    self.fig, self.plots = plt.subplots(rows, cols, figsize=(17, rows * 3), sharex=True, sharey=True)
+    self.fig.suptitle('Test Prediction vs Test samples', fontsize=15, color='forestgreen')
+    self.fig.tight_layout(pad=3.0)
+
+    self.plots = self.plots.flatten()
+
+    
 
 class TestingPlotCallback(tf.keras.callbacks.Callback):
   def __init__(self, steps = 5, epochs_step=10, test_data=None):
@@ -9,12 +28,27 @@ class TestingPlotCallback(tf.keras.callbacks.Callback):
     
     rows = (steps + 4) // 5
     cols = 5
+    self.fig, self.plots = plt.subplots(rows, cols, figsize=(17, rows * 3), sharex=True, sharey=True)
+    # self.fig = plt.figure(figsize=(20, rows * 2))
+    self.fig.suptitle('Test Prediction vs Test samples', fontsize=15, color='forestgreen')
+    self.fig.tight_layout(pad=3.0)
+    self.fig.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5)
 
-    self.fig = plt.figure(figsize=(20, rows * 2))
-    self.fig.suptitle('Test Prediction vs Test samples', fontsize=10, color='forestgreen')
-    
-    self.plots = self.fig.subplots(rows, cols).flatten()
+    self.plots = self.plots.flatten()
     self.test_data = test_data
+
+    y_min, y_max = min(test_data[1])[0], max(test_data[1])[0]
+    yspread = y_max - y_min
+
+    self.lims = {
+      "y": (
+        floor(y_min - 0.1 * yspread),
+        floor(y_max + 0.1 * yspread),
+        floor(yspread * 1.2 // 5)
+      )
+    }
+
+    print(self.lims)
 
   def on_epoch_end(self, epoch, logs={}):
     mark = self.epochs_step - 1
@@ -26,9 +60,11 @@ class TestingPlotCallback(tf.keras.callbacks.Callback):
       ax.scatter(test_x, test_y, label='test', color='red')
       ax.plot(test_x, self.model.predict(test_x), label='prediction')
       ax.legend(['Test', 'Prediction'])
+      ax.set_title('Epoch: {}'.format(epoch))
 
+      y_min, y_max, y_step = self.lims["y"]
       ax.set_xticks(np.arange(-8, 10, 2))
-      ax.set_yticks([])
+      ax.set_yticks(np.arange(y_min, y_max, y_step))
       self.fig.add_subplot(ax)
 
 class LossPlotCallback(tf.keras.callbacks.Callback):
@@ -38,8 +74,9 @@ class LossPlotCallback(tf.keras.callbacks.Callback):
 
     rows = (steps + 4) // 5
     cols = 5
-    self.fig = plt.figure(figsize=(20, rows * 2))
-    self.fig.suptitle('Loss', fontsize=20, color='forestgreen')
+    self.fig = plt.figure(figsize=(17, rows * 2.3))
+    self.fig.suptitle('Loss', fontsize=15, color='forestgreen')
+    self.fig.tight_layout(pad=3.0)
     
     self.plots = self.fig.subplots(rows, cols).flatten()
 
@@ -57,7 +94,7 @@ class LossPlotCallback(tf.keras.callbacks.Callback):
       ax.legend(['Loss'])
 
       ax.set_xticks(np.arange(0, len(self.losses), 5 * (step + 1)))
-      ax.set_yticks([])
+      ax.set_ylim(0, max(self.losses) * 1.1)
       self.fig.add_subplot(ax)
 
 
